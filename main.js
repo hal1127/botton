1e7df462eb391de8a19837ff97bf0faf33f827a6
hal1127 <harukium@gmail.com>
2021-01-04 05:22:12 +0900

    Initialize repository

---
+++ main.js
@@ -0,0 +1,59 @@
+'use strict';
+$(function() {
+  let basePath = window.location.origin;
+  let remain;
+  let btn = $('#btn')[0];
+  $.ajax(basePath+'/app/', 
+  {
+    type: 'get',
+    data: {method: 'show'},
+    dataType: 'json',
+  })
+  .done(function(data) {
+    remain = data[0].remain;
+    btn.innerHTML = remain === '0' ? 'おめでとう!' : remain;
+  })
+  .fail(function() {
+    alert("接続に失敗しました。");
+  });
+
+  $('#btn').on('click', function(e) {
+    if (remain === '0') {
+      btn.innerHTML = 'おめでとう!'
+    } else {
+      $.ajax(basePath+'/app/', 
+      {
+        type: 'post',
+        data: {method: 'push'},
+        dataType: 'json',
+      })
+      .done(function(data) {
+        remain = data[0].remain;
+        e.target.innerHTML = remain;
+      })
+      .fail(function() {
+        alert("接続に失敗しました。");
+      });
+    }
+  });
+  
+  let intervalId = setInterval(function() {
+    if (remain === '0') {
+      clearInterval(intervalId);
+    }
+    $.ajax(basePath+'/app/', 
+    {
+      type: 'get',
+      data: {method: 'show'},
+      dataType: 'json',
+    })
+    .done(function(data) {
+      // console.log($('#btn'));
+      remain = data[0].remain;
+      btn.innerHTML = remain;
+    })
+    .fail(function() {
+      alert("接続に失敗しました。");
+    })
+  }, 1000)
+});
